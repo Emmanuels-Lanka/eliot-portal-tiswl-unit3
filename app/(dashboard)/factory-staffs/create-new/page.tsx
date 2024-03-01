2c6c643872343973407bd2bef48c5b1d79364739
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2024-03-01 12:13:47 +0530

    Factory Staff ✅

---
+++ app/(dashboard)/factory-staffs/create-new/page.tsx
@@ -0,0 +1,7 @@
+import AddFactoryStaffForm from '@/components/dashboard/forms/add-factory-staff-form'
+
+const CreateNewFactoryStaff = () => {
+  return <AddFactoryStaffForm />
+}
+
+export default CreateNewFactoryStaff
---
+++ app/(dashboard)/factory-staffs/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FactoryStaff = () => {
+  return (
+    <div>FactoryStaff</div>
+  )
+}
+
+export default FactoryStaff
