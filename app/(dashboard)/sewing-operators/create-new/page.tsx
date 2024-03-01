61a72f17e167e9e26dcbb57ee4b97d3e7cf457fc
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2024-03-01 12:14:53 +0530

    Sewing Operators ✅

---
+++ app/(dashboard)/sewing-operators/create-new/page.tsx
@@ -0,0 +1,7 @@
+import AddSewingOperatorForm from '@/components/dashboard/forms/add-sewing-operator-form'
+
+const CreateNewSewingOperator = () => {
+  return <AddSewingOperatorForm />
+}
+
+export default CreateNewSewingOperator
---
+++ app/(dashboard)/sewing-operators/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SewingOperators = () => {
+  return (
+    <div>SewingOperators</div>
+  )
+}
+
+export default SewingOperators
