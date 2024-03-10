76f96af2bf74678f10e6e40b7dddbeac769ebea5
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2024-03-10 10:19:18 +0530

    Modified components 🛠️

--- components/dashboard/dashboard-header.tsx
+++ components/dashboard/dashboard-header.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { usePathname } from 'next/navigation';
+import { Cog } from 'lucide-react';
 
 import { HEADER_INFO } from '@/constants';
 
@@ -10,12 +11,18 @@ const DashboardHeader = () => {
 
   return (
     <header className='w-full h-[60px] px-4 bg-white flex justify-between items-center'>
-      {filteredData.map((item) => (
+      {filteredData && filteredData.map((item) => (
         <div key={item.href} className='flex items-center gap-3'>
           <item.icon className='w-7 h-7 text-voilet' />
           <h1 className='text-slate-500'>{item.label}</h1>
         </div>
       ))}
+      {filteredData.length === 0 && (
+        <div className='flex items-center gap-3'>
+          <Cog className='w-7 h-7 text-voilet' />
+          <h1 className='text-slate-500'>ELIoT Web Portal</h1>
+        </div>
+      )}
     </header>
   )
 }
