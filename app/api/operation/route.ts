1f270b4624228d19e51849851f2e4682966c6c6f
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2024-03-11 11:41:30 +0530

    Operations ✅

---
+++ app/api/operation/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+
+export async function POST(
+    req: Request,
+) {
+    try {
+        const { name } = await req.json();
+
+        const existingOperationByName = await db.operation.findUnique({
+            where: {
+                name
+            }
+        });
+
+        if (existingOperationByName) {
+            return new NextResponse("Already created a operation with this same name! please use different one", { status: 409 })
+        }
+
+        const newOperation = await db.operation.create({
+            data: {
+                name
+            }
+        });
+
+        return NextResponse.json({ data: newOperation, message: 'Operation created successfully'}, { status: 201 });
+
+    } catch (error) {
+        console.error("[OPERATION_ERROR]", error);
+        return new NextResponse("Internal Error", { status: 500 });
+    }
+}
