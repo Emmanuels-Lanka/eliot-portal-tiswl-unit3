6299dfb1f9cac0f893a768bcbf6cffca14df915b
Vinojan1999 <vinojan02abhimanyu@gmail.com>
2024-03-13 19:32:21 +0530

    Added loadding state 🔄

---
+++ app/(dashboard)/sewing-machines/loading.tsx
@@ -0,0 +1,11 @@
+import { Loader2 } from "lucide-react";
+
+const MachineLoading = () => {
+    return (
+        <div className='loading-body-height w-full flex justify-center items-center'>
+            <Loader2 className="animate-spin w-8 h-8" />
+        </div>
+    )
+}
+
+export default MachineLoading;
