123cf8f5bfd2626eb7beed8ebc4a85b44514b110
chrishanthacosta <64259523+chrishanthacosta@users.noreply.github.com>
2024-08-16 08:46:17 +0530

    ach-rate-operation- sql query

--- app/(dashboard)/analytics/achievement-rate-operation/_components/actions.ts
+++ app/(dashboard)/analytics/achievement-rate-operation/_components/actions.ts
@@ -5,13 +5,23 @@ import { ProductionDataType } from "./analytics-chart";
 export async function getOperatorEfficiency(obbsheetid:string,date:string) : Promise<ProductionDataType[]>   {
     const sql = neon(process.env.DATABASE_URL || "");
 
-    const data = await sql`SELECT sum(pd."productionCount") as count,o.name  ,oo.target
+    // const data1 = await sql`SELECT sum(pd."productionCount") as count,o.name  ,oo.target
+    //         FROM "ProductionData" pd
+    //         INNER JOIN "ObbOperation" oo ON pd."obbOperationId" = oo.id
+    //         INNER JOIN "ObbSheet" os ON oo."obbSheetId" = os.id
+    //         INNER JOIN "Operation" o ON o.id= pd."obbOperationId"
+    //         WHERE os.id = ${obbsheetid} and pd.timestamp like ${date}
+    //         group by o.name,oo."seqNo",oo.target order by  oo."seqNo" ;`;
+    
+     const data = await sql`SELECT opn.name,    sum(pd."productionCount")  as count, obbopn.target
             FROM "ProductionData" pd
-            INNER JOIN "ObbOperation" oo ON pd."obbOperationId" = oo.id
-            INNER JOIN "ObbSheet" os ON oo."obbSheetId" = os.id
-            INNER JOIN "Operation" o ON o.id= pd."obbOperationId"
-            WHERE os.id = ${obbsheetid} and pd.timestamp like ${date}
-            group by o.name,oo."seqNo",oo.target order by  oo."seqNo" ;`;
-    console.log("data",data,)
+            INNER JOIN "ObbOperation" obbopn ON pd."obbOperationId" = obbopn.id
+            INNER JOIN "Operation" opn ON opn.id= obbopn."operationId"
+            INNER JOIN "ObbSheet" obbs ON obbopn."obbSheetId" = obbs.id
+            WHERE   pd.timestamp like  ${date} and  obbs.id = ${obbsheetid}
+            group by opn.name,obbopn."seqNo",obbopn.target order by  obbopn."seqNo" `
+    
+    
+    console.log("data",data,obbsheetid)
     return new Promise((resolve) => resolve(data as ProductionDataType[] ))
 }
