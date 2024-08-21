37c74bd783bf7362d2266301f492261c80b7a463
chrishanthacosta <chrishanthacosta@gmail.com>
2024-08-21 22:22:40 +0530

    prod count ok 15min hmap

--- app/(dashboard)/analytics/operation-efficiency-15/_components/actions.ts
+++ app/(dashboard)/analytics/operation-efficiency-15/_components/actions.ts
@@ -5,7 +5,7 @@ import { neon } from "@neondatabase/serverless";
 export async function getData(obbsheetid:string,date:string)  : Promise<any[]>{
     const sql = neon(process.env.DATABASE_URL || "");
  
-    const data = await sql`SELECT pd."productionCount" as count,concat(oo."seqNo",'-',o.name ) as name  , pd.timestamp as timestamp, oo."seqNo"
+    const data = await sql`SELECT pd."productionCount" as count, substring(concat(oo."seqNo",'-',o.name ) from 0 for 20) as name  , pd.timestamp as timestamp, oo."seqNo"
     FROM "ProductionData" pd
     right JOIN "ObbOperation" oo ON pd."obbOperationId" = oo.id
     INNER JOIN "ObbSheet" os ON oo."obbSheetId" = os.id
@@ -13,7 +13,7 @@ export async function getData(obbsheetid:string,date:string)  : Promise<any[]>{
     WHERE os.id = ${obbsheetid} and pd.timestamp like ${date}
      order by  pd.timestamp ;`;
 
-    console.log("data fetched",data,)
+    console.log("data fetched",data.length,)
 
 
  
