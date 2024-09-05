9ae814c82760066a5c89a4b74799fea3f351685f
Your Name <adilsaaly@gmail.com>
2024-09-06 00:24:40 +0530

    "Updated heatmap components

---
+++ app/(tv-charts)/g2/[linename]/_components/tv-achievement-rate-operation.tsx
@@ -0,0 +1,54 @@
+"use client"
+import { useEffect, useState } from "react";
+import { getObbSheetID } from "./actions";
+import { Span } from "next/dist/trace";
+import DashboardHeader from "@/components/dashboard/dashboard-header";
+import { Cog } from "lucide-react";
+import BarChartGraph from "./bar-chart-graph";
+import LogoImporter from "@/components/dashboard/common/eliot-logo";
+
+
+const AchievementRateOperation = ({ linename }: { linename: string }) => {
+
+  const [obbSheetId, setobbSheetId] = useState<string>("")
+  const [date, setdate] = useState<string>("")
+
+  const getObbSheetID1 = async () => {
+    const obbSheetId1 = await getObbSheetID(linename);
+    setobbSheetId(obbSheetId1)
+   
+  }
+
+  useEffect(() => {
+
+    const y = new Date().getFullYear().toString()
+    const m = (new Date().getMonth() + 1).toString().padStart(2, "0")
+    //const d = new Date().getDate().toString().padStart(2, "0")
+    const today = new Date();
+    const yyyyMMdd = today.getFullYear() + '-' + (today.getMonth() + 1).toString().padStart(2, '0') + '-' + today.getDate().toString().padStart(2, '0');
+  
+   const date =  yyyyMMdd.toString()+"%"
+    setdate(date)
+
+  }, [])
+
+  useEffect(() => {
+    console.log("linename", linename,)
+    getObbSheetID1()
+  }, [linename])
+  return (
+    <div className="h-[200]">
+      <div className='flex justify-center items-center gap-3'>
+        {/* <Cog className='w-7 h-7 text-voilet' /> */}
+        <LogoImporter/> 
+        <h1 className='text-[#0071c1] mx-4 text-3xl'>Dashboard - Operation Overall Achievement for {linename}</h1>
+      </div>
+
+      {obbSheetId.length > 0 ? <BarChartGraph
+        obbSheetId={obbSheetId}
+        date={date}
+      /> : <span>No Layout for Line {linename} - {date}</span>}
+    </div>
+  )
+}
+export default AchievementRateOperation;
