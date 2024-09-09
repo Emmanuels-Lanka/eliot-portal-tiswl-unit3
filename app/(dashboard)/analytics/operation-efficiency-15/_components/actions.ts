236b81264461736303f8146f3038f88eb9361d81
Your Name <adilsaaly@gmail.com>
2024-09-09 09:59:48 +0530

    action change done

--- app/(dashboard)/analytics/operation-efficiency-15/_components/actions.ts
+++ app/(dashboard)/analytics/operation-efficiency-15/_components/actions.ts
@@ -5,11 +5,13 @@ import { neon } from "@neondatabase/serverless";
 export async function getData(obbsheetid:string,date:string)  : Promise<any[]>{
     const sql = neon(process.env.DATABASE_URL || "");
     //,pd."eliotSerialNumber" as eliotid
-    const data = await sql`SELECT pd."productionCount" as count, substring(concat(oo."seqNo",'-',o.name ) from 0 for 20) as name  , pd.timestamp as timestamp, oo."seqNo"
+    const data = await sql`SELECT pd."productionCount" as count, concat(oo."seqNo",'-',o.name, '-' , sm."machineId") as name  , pd.timestamp as timestamp, oo."seqNo"
     FROM "ProductionData" pd
     right JOIN "ObbOperation" oo ON pd."obbOperationId" = oo.id
     INNER JOIN "ObbSheet" os ON oo."obbSheetId" = os.id
     inner JOIN "Operation" o ON o.id= oo."operationId"
+    inner JOIN "SewingMachine" sm ON sm."id"= oo."sewingMachineId"
+
     WHERE os.id = ${obbsheetid} and pd.timestamp like ${date}
      order by  pd.timestamp ;`;
 
@@ -24,12 +26,21 @@ export async function geOperationList(obbsheetid:string ) : Promise<any[]>  {
     const sql = neon(process.env.DATABASE_URL || "");
  
     // const data = await sql`SELECT   concat(oo."seqNo",'-',o.name ) as name
-    const data = await sql`SELECT substring(concat(oo."seqNo",'-',o.name ) from 0 for 20)  as name
+    const data = await sql`SELECT concat(oo."seqNo",'-',o.name, '-' , sm."machineId" ) as name
     FROM "ObbOperation" oo  
     INNER JOIN "ObbSheet" os ON oo."obbSheetId" = os.id
+    inner JOIN "SewingMachine" sm ON sm."id"= oo."sewingMachineId"
+
     inner JOIN "Operation" o ON o.id= oo."operationId"
+
+
+
+    
     WHERE os.id = ${obbsheetid}  
      order by  oo."seqNo" ;`;
+
+
+     console.log("asdasdasd",obbsheetid)
 
    
 
