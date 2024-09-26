bb50bf890eb434ae260b8be139b963a22835ce01
Your Name <adilsaaly@gmail.com>
2024-09-26 11:04:56 +0530

    "Update version number in dashboard footer from v1.0.d17.14 to v1.0.d17.15"

--- components/dashboard/dashboard-footer.tsx
+++ components/dashboard/dashboard-footer.tsx
@@ -7,7 +7,7 @@ const DashboardFooter = () => {
         </p>
         <div className='flex items-end gap-1'>
             <p className='text-sm text-slate-600 font-semibold'>ELIoT Web Portal</p>
-            <p className='hidden md:flex text-[10px] text-slate-500'>v1.0.d17.14</p>
+            <p className='hidden md:flex text-[10px] text-slate-500'>v1.0.d17.15</p>
         </div>
     </footer>
   )
