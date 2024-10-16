12503e42f7aee4f6e8b4975bc488704d3d16d27e
Your Name <adilsaaly@gmail.com>
2024-10-16 20:39:10 +0530

    portal chart done

--- app/(dashboard)/analytics/efficiency-rate/_components/analytics-chart.tsx
+++ app/(dashboard)/analytics/efficiency-rate/_components/analytics-chart.tsx
@@ -19,11 +19,18 @@ interface AnalyticsChartProps {
 }
 
 export type ProductionDataType = {
+    namee: string;
+    times: any;
+    logout: null;
+    login?:any;
+    avg: number;
     seqNo?: string;
     seqno?: string;
     name: string;
     count: number;
     target: number;
+    first:any;
+    last:any;
 }
 
 const EfficiencyAnalyticsChart = ({
@@ -39,80 +46,7 @@ const EfficiencyAnalyticsChart = ({
     const[date,setDate]=useState<string>("")
     
 
-    function processForBarchart({ productionData, hourGroup }: { productionData: ProductionData[]; hourGroup: string }) {
-        const hourRanges: Record<string, [number, number]> = {
-            "1": [7, 8],
-            "2": [8, 9],
-            "3": [9, 10],
-            "4": [10, 11],
-            "5": [11, 12],
-            "6": [12, 13],
-            "7": [13, 14],
-            "8": [14, 15],
-            "9": [15, 16],
-            "10": [16, 17],
-            "11": [17, 18],
-            "12": [18, 19],
-        };
-
-        const [startHour, endHour] = hourRanges[hourGroup];
-
-        const filteredData = productionData.filter(data => {
-            const productionHour = new Date(data.timestamp).getHours();
-            return productionHour >= startHour && productionHour < endHour;
-        });
-
-        const productionCountMap: Record<string, { count: number; target: number }> = {};
-
-        filteredData.forEach((data: any) => {
-            const operationName = data.obbOperation?.operation?.name;
-            const target = data.obbOperation?.target ?? 0;
-            if (operationName) {
-                if (!productionCountMap[operationName]) {
-                    productionCountMap[operationName] = { count: 0, target };
-                }
-                productionCountMap[operationName].count += data.productionCount;
-                productionCountMap[operationName].target = target; // Assuming target is the same for all entries with the same operation
-            }
-        });
-
-        const barchartData: ProductionDataType[] = Object.keys(productionCountMap).map(operationName => ({
-            name: operationName,
-            count: productionCountMap[operationName].count,
-            target: productionCountMap[operationName].target,
-        }));
-
-        return barchartData;
-    }
-
-    // const handleFetchProductions = async (data: { obbSheetId: string; date: Date; hourGroup: string }) => {
-    //     try {
-    //         data.date.setDate(data.date.getDate() + 1);
-    //         const formattedDate = data.date.toISOString().split('T')[0];
-            
-    //         const response = await axios.get(`/api/getobb-date?obbSheetId=${data.obbSheetId}&date=${formattedDate}`);
-    //         console.log("DATA:", response.data.data);
-            
-    //         const barchartData = processForBarchart({ productionData: response.data.data, hourGroup:data.hourGroup });
-    //         console.log("DATA:", barchartData);
-    //         setProduction(barchartData);
-    //         router.refresh();
-    //     } catch (error: any) {
-    //         console.error("Error fetching production data:", error);
-    //         toast({
-    //             title: "Something went wrong! Try again",
-    //             variant: "error",
-    //             description: (
-    //                 <div className='mt-2 bg-slate-200 py-2 px-3 md:w-[336px] rounded-md'>
-    //                     <code className="text-slate-800">
-    //                         ERROR: {error.message}
-    //                     </code>
-    //                 </div>
-    //             ),
-    //         });
-    //         console.error(error)
-    //     }
-    // }
+   
     
     const Fetchdata = async (data: { obbSheetId: string; date: Date }) => {
         try {
