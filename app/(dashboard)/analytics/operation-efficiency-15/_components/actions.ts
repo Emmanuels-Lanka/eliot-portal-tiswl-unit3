a4704c5318dca714a2e77611b5cc9025c7b21cd9
Your Name <adilsaaly@gmail.com>
2025-01-04 19:14:22 +0530

    15min prod done

--- app/(dashboard)/analytics/operation-efficiency-15/_components/actions.ts
+++ app/(dashboard)/analytics/operation-efficiency-15/_components/actions.ts
@@ -1,74 +1,117 @@
 "use server";
-import { neon } from "@neondatabase/serverless";
+import { createPostgresClient } from "@/lib/postgres";
 
 
 export async function getData(obbsheetid:string,date:string)  : Promise<any[]>{
-    const sql = neon(process.env.DATABASE_URL || "");
-    //,pd."eliotSerialNumber" as eliotid
-    const data = await sql`SELECT pd."productionCount" as count, concat(o.name, ' - ' ,'(', sm."machineId",')',' - ',oo."seqNo") as name  , pd.timestamp as timestamp, oo."seqNo"
+
+    {
+        const client = createPostgresClient();
+      try {
+    
+        await client.connect();
+        const query = `
+          SELECT pd."productionCount" as count, concat(o.name, ' - ' ,'(', sm."machineId",')',' - ',oo."seqNo") as name  , pd.timestamp as timestamp, oo."seqNo"
     FROM "ProductionData" pd
     right JOIN "ObbOperation" oo ON pd."obbOperationId" = oo.id
     INNER JOIN "ObbSheet" os ON oo."obbSheetId" = os.id
     inner JOIN "Operation" o ON o.id= oo."operationId"
     inner JOIN "SewingMachine" sm ON sm."id"= oo."sewingMachineId"
 
-    WHERE os.id = ${obbsheetid} and pd.timestamp like ${date}
-     order by  pd.timestamp ;`;
-
- 
-
-
- 
-    return new Promise((resolve) => resolve(data ))
+    WHERE os.id = $1 and pd.timestamp like $2
+     order by  pd.timestamp ;
+        `;
+        const values = [obbsheetid,date];
+    
+        const result = await client.query(query, values);
+    
+        // console.log("DATAaa: ", result.rows);
+        return new Promise((resolve) => resolve(result.rows ));
+        
+        
+      } catch (error) {
+        console.error("[TEST_ERROR]", error);
+        throw error;
+      }
+      finally{
+        await client.end()
+      }}
 }
 
 export async function geOperationList(obbsheetid:string,date:string) : Promise<any[]>  {
-    const sql = neon(process.env.DATABASE_URL || "");
- 
-    // const data = await sql`SELECT   concat(oo."seqNo",'-',o.name ) as name
-    const data = await sql`SELECT concat(o.name, ' - ' ,'(', sm."machineId",')',' - ',oo."seqNo") as name
+
+    {
+        const client = createPostgresClient();
+      try {
+    
+        await client.connect();
+        const query = `
+          SELECT concat(o.name, ' - ' ,'(', sm."machineId",')',' - ',oo."seqNo") as name
     FROM "ObbOperation" oo  
     INNER JOIN "ObbSheet" os ON oo."obbSheetId" = os.id
     inner JOIN "SewingMachine" sm ON sm."id"= oo."sewingMachineId"
     inner Join "ProductionData" pd ON pd."obbOperationId" = oo.id
     inner JOIN "Operation" o ON o.id= oo."operationId"
-
-
-
-    
-    WHERE os.id = ${obbsheetid}  and pd.timestamp like ${date}
+    WHERE os.id = $1  and pd.timestamp like $2
       GROUP BY  concat(o.name, ' - ' ,'(', sm."machineId",')',' - ',oo."seqNo"),oo."seqNo"
-    order by oo."seqNo" ;`;
+    order by oo."seqNo" ;
+        `;
+        const values = [obbsheetid,date];
+    
+        const result = await client.query(query, values);
+    
+        // console.log("DATAaa: ", result.rows);
+        return new Promise((resolve) => resolve(result.rows ));
+        
+        
+      } catch (error) {
+        console.error("[TEST_ERROR]", error);
+        throw error;
+      }
+      finally{
+        await client.end()
+      }}
 
-
-     console.log("asdasdasd",obbsheetid)
-
-   
 
 
  
-    return new Promise((resolve) => resolve(data ))
+    
 }
 
 export async function getEliotMachineList(obbsheetid:string ,date:string) : Promise<any[]>  {
-    const sql = neon(process.env.DATABASE_URL || "");
- 
-    // const data = await sql`SELECT   concat(oo."seqNo",'-',o.name ) as name
 
-
- 
- 
-     const data = await sql`SELECT sm."machineId",ed."serialNumber"
+    {
+        const client = createPostgresClient();
+      try {
+    
+        await client.connect();
+        const query = `
+          SELECT sm."machineId",ed."serialNumber"
     FROM "ObbOperation" oo  
     INNER JOIN "ObbSheet" os ON oo."obbSheetId" = os.id
     inner JOIN "SewingMachine" sm ON sm."id"= oo."sewingMachineId"
     inner JOIN "EliotDevice" ed ON ed.id = sm."eliotDeviceId"
     inner Join "ProductionData" pd ON pd."obbOperationId" = oo.id
-    WHERE os.id = ${obbsheetid}  and pd.timestamp like ${date}
+    WHERE os.id = $1  and pd.timestamp like $2
     group by sm."machineId",ed."serialNumber",oo."seqNo"
-     order by  oo."seqNo" ;`;
+     order by  oo."seqNo" ;
+        `;
+        const values = [obbsheetid,date];
+    
+        const result = await client.query(query, values);
+    
+        // console.log("DATAaa: ", result.rows);
+        return new Promise((resolve) => resolve(result.rows ));
+        
+        
+      } catch (error) {
+        console.error("[TEST_ERROR]", error);
+        throw error;
+      }
+      finally{
+        await client.end()
+      }}
 
 
- 
-    return new Promise((resolve) => resolve(data ))
+
+
 }
