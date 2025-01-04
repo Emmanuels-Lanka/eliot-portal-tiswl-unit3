9fdc4b993eec917b298fbc77b618818dcc312d35
Your Name <adilsaaly@gmail.com>
2025-01-04 21:14:26 +0530

    log done

--- components/log/action.ts
+++ components/log/action.ts
@@ -1,12 +1,19 @@
 "use server";
 import { neon } from "@neondatabase/serverless";
 import { ProductionDataType } from "./LogTable";
+import { createPostgresClient } from "@/lib/postgres";
 
 
 export async function getData(obbsheetid:string,date:string)  : Promise<ProductionDataType[]>   {
-    const sql = neon(process.env.DATABASE_URL || "");
 
-    const data = await sql`SELECT 
+
+  {
+    const client = createPostgresClient();
+  try {
+
+    await client.connect();
+    const query = `
+      SELECT 
   oprt.name, 
   oprt."employeeId", 
   opn.name AS operationname, 
@@ -27,8 +34,8 @@ FROM
   INNER JOIN "Operator" oprt ON oprt.rfid = pd."operatorRfid"
   
 WHERE 
-  pd.timestamp LIKE ${date} 
-  AND obbs.id = ${obbsheetid}
+  pd.timestamp LIKE $2
+  AND obbs.id = $1
   
 GROUP BY 
   sm."machineId", 
@@ -41,66 +48,31 @@ GROUP BY
   oprt."employeeId",
   obbopn."seqNo"
   ORDER BY 
-  obbopn."seqNo";`;
+  obbopn."seqNo";
+    `;
+    const values = [obbsheetid,date];
 
+    const result = await client.query(query, values);
+
+    // console.log("DATAaa: ", result.rows);
+    return new Promise((resolve) => resolve(result.rows as ProductionDataType[] ));
+    
+    
+  } catch (error) {
+    console.error("[TEST_ERROR]", error);
+    throw error;
+  }
+  finally{
+    await client.end()
+  }}
+
+
+   
      
 
 
- 
-    return new Promise((resolve) => resolve(data as ProductionDataType[]  ))
 }
 
 
 
 
-// "use server";
-// import { neon } from "@neondatabase/serverless";
-// import { ProductionDataType } from "./LogTable";
-
-
-// export async function getData(obbsheetid:string,date:string)  : Promise<ProductionDataType[]>   {
-//     const sql = neon(process.env.DATABASE_URL || "");
-
-//     const data = await sql`SELECT 
-//   oprt.name, 
-//   oprt."employeeId", 
-//   opn.name AS operationname, 
-//   opn.code, 
-//   pd."eliotSerialNumber", 
-//   SUM(pd."productionCount") AS totprod, 
-//   obbopn.target, 
-//   obbopn."seqNo", 
-//   sm."machineId", 
-//   null as "LoginTimestamp", 
-//   null  as "LogoutTimestamp"
-// FROM 
-//   "ProductionData" pd
-//   INNER JOIN "ObbOperation" obbopn ON pd."obbOperationId" = obbopn.id
-//   INNER JOIN "Operation" opn ON opn.id = obbopn."operationId"
-//   INNER JOIN "ObbSheet" obbs ON obbopn."obbSheetId" = obbs.id
-//   INNER JOIN "SewingMachine" sm ON sm.id = obbopn."sewingMachineId"
-//   INNER JOIN "Operator" oprt ON oprt.rfid = pd."operatorRfid"
-//   INNER JOIN "OperatorSession" opses ON opses."operatorRfid" = oprt."rfid"
-// WHERE 
-//   pd.timestamp LIKE ${date} 
-//   AND obbs.id = ${obbsheetid}
-//   AND  opses."LoginTimestamp" LIKE ${date} 
-// GROUP BY 
-//   sm."machineId", 
-//   pd."eliotSerialNumber", 
-//   opn.name, 
-//   opn.code, 
-//   obbopn."seqNo", 
-//   obbopn.target, 
-//   oprt.name, 
-//   oprt."employeeId",
-//   obbopn."seqNo"
-//   ORDER BY 
-//   obbopn."seqNo";`;
-
-     
-
-
- 
-//     return new Promise((resolve) => resolve(data as ProductionDataType[]  ))
-// }
