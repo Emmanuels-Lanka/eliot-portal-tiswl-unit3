fdfce9c11e08e8b6abceefa49e3fc94c4322a514
Vinojan Veerapathrathasan <imvinojanv@gmail.com>
2025-01-13 20:15:20 +0530

    updated test changes

--- app/api/v1/test/data/delete/route.ts
+++ app/api/v1/test/data/delete/route.ts
@@ -4,13 +4,23 @@ import { db } from "@/lib/db";
 
 export async function DELETE() {
     try {
-        const data = await db.obbSheet.findMany({
-            select: {
-                name: true,
-                productionLine: true
+        const data = await db.productionData.count({
+            where: {
+                // timestamp: {
+                //     gte: "2025-01-10 00:00:00"
+                // },
+                eliotSerialNumber: null
             }
         });
-        // const data = await db.eliotDevice.deleteMany();
+
+        // const data = await db.productionData.deleteMany({
+        //     where: {
+        //         timestamp: {
+        //             lte: "2024-12-10 00:00:00"
+        //         },
+        //         // eliotSerialNumber: null
+        //     }
+        // });
 
         return NextResponse.json(data, { status: 201 });
     } catch (error) {
