7c64ad96101360d8cca50979e525cbf4172ee250
Vinojan Veerapathrathasan <imvinojanv@gmail.com>
2025-01-15 11:47:39 +0530

    Fixed aws issues

--- app/api/v1/test/data/fetch/route.ts
+++ app/api/v1/test/data/fetch/route.ts
@@ -12,28 +12,28 @@ export async function GET() {
     const take = pageSize;
 
     try {
-        const data = await db.productionData.findMany({
-            // where: {
-            //     timestamp: {
-            //         gte: "2025-01-10 00:00:00"
-            //     },
-            //     eliotSerialNumber: { not: null }
-            // },
-            orderBy: {
-                createdAt: "asc"
-            },
-            // take,
-            // skip
-        });
-        console.log("COUNT:", data.length);
+        // const data = await db.productionData.findMany({
+        //     // where: {
+        //     //     timestamp: {
+        //     //         gte: "2025-01-10 00:00:00"
+        //     //     },
+        //     //     eliotSerialNumber: { not: null }
+        //     // },
+        //     orderBy: {
+        //         createdAt: "asc"
+        //     },
+        //     take,
+        //     skip
+        // });
+        // console.log("COUNT:", data.length);
 
         // Define the path for the JSON file
-        const filePath = "D:/Documents/EL/eliot-portal/app/api/v1/test/data/pro-data-10.json";
+        // const filePath = "D:/Documents/EL/eliot-portal/app/api/v1/test/data/pro-data-10.json";
 
         // Write the data to a JSON file
-        await fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8');
+        // await fs.writeFile(filePath, JSON.stringify(data, null, 2), 'utf8');
 
-        return NextResponse.json(data.length, { status: 201 });
+        return NextResponse.json("data.length", { status: 201 });
     } catch (error) {
         console.error("[UPDATE_DATA_ERROR]", error);
         return new NextResponse("Internal Error", { status: 500 });
