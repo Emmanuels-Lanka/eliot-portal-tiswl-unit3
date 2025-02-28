b9ff8f83ebe3298532a4f0124fad1fa9de9d4e1d
Your Name <adilsaaly@gmail.com>
2025-03-01 00:26:20 +0530

    efficiency Live  tv chart can choose its mode

--- app/(tv-charts)/efficiency-live/page.tsx
+++ app/(tv-charts)/efficiency-live/page.tsx
@@ -1,5 +1,7 @@
 import { db } from '@/lib/db';
 import SelectObbSheet from '../line-dashboard/_components/select-obb-sheet';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
 
 const Efficiency = async () => {
     const obbSheets = await db.obbSheet.findMany({
@@ -7,12 +9,34 @@ const Efficiency = async () => {
         orderBy: { createdAt: "desc" },
     });
 
+            
     return (
         <div className='mx-auto max-w-4xl'>
+             <Tabs defaultValue="1" className="w-full">
+        <div className='flex flex-col justify-center mt-12 items-center'>
+          <TabsList className='shadow-md px-4'>
+            <TabsTrigger value="1">Live Efficiency</TabsTrigger>
+            <TabsTrigger value="2">Cumilative Efficiency</TabsTrigger>
+          </TabsList>
+          <p className="mt-4 text-sm text-slate-500">
+            (Please click the tabs to change the heatmaps)
+          </p>
+              </div>
+          <TabsContent value="1">
+           
             <div className='mt-24'>
-                <h1 className='text-center text-2xl font-medium text-sky-700 mb-6'>Select Obb Sheet for this Line</h1>
+                <h1 className='text-center text-2xl font-medium text-sky-700 mb-6'>Select Obb Sheet for Live Efficiency</h1>
                 <SelectObbSheet obbSheets={obbSheets} route="/efficiency-live" />
             </div>
+          </TabsContent>
+          <TabsContent value="2">
+          <div className='mt-24'>
+                <h1 className='text-center text-2xl font-medium text-sky-700 mb-6'>Select Obb Sheet for Cumilative Efficiency</h1>
+                <SelectObbSheet obbSheets={obbSheets} route="/cumilative-efficiency" />
+            </div>
+          </TabsContent>
+         
+        </Tabs>
         </div>
     )
 }
