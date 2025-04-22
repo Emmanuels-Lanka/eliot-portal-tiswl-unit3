300591a67d0cedfc235a15234b68f5143fca389c
Ravishka Thushan <ravishkathushan.dev@gmail.com>
2025-04-22 10:32:32 +0530

    roi-calculator report

--- app/roi-calculator/page.tsx
+++ app/roi-calculator/page.tsx
@@ -10,6 +10,7 @@ const page = () => {
         <Image src={"/eliot-logo.png"} alt=' ' height={150} width={150} />
         <h1 className='text-[#0071c1] my-4 text-2xl'>Return of Investment Calculator</h1>
       </div>
+      
       <CalculatorForm/>
     </div>
     </>
