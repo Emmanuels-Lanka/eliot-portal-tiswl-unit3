056e93a63f4ff341502f93dcc82a009ae264d273
ImAadhi <adilsaaly@gmail.com>
2025-05-26 10:04:07 +0530

    Operation Efficiency (60 Minute) Eff accoding to obb Done

--- app/(dashboard)/analytics/operation-efficiency-60/_components/cumilative-analytics.tsx
+++ app/(dashboard)/analytics/operation-efficiency-60/_components/cumilative-analytics.tsx
@@ -19,6 +19,12 @@ interface AnalyticsChartProps {
     }[] | null;
     title: string;
 }
+type ObbSheetEffType = {
+    name: string;
+    efficiencyLevel1: number;
+    efficiencyLevel2: number;
+    efficiencyLevel3: number;
+} | null;
 
 type ProductionDataForChartTypes = {
     id: string;
@@ -80,7 +86,7 @@ const OperationCumilative = ({
 
     const [heatmapData, setHeatmapData] = useState<OperationEfficiencyOutputTypes>();
     const [obbSheet, setObbSheet] = useState<ObbSheet | null>(null);
-function processProductionData(productionData: ProductionDataForChartTypes[]): OperationEfficiencyOutputTypes {
+function processProductionData(productionData: ProductionDataForChartTypes[],obbSheet:ObbSheetEffType): OperationEfficiencyOutputTypes {
         const hourGroups = ["8:00 AM - 9:00 AM", "9:00 AM - 10:00 AM", "10:00 AM - 11:00 AM", "11:00 AM - 12:00 PM", "12:00 PM - 1:00 PM", "1:00 PM - 2:00 PM", "2:00 PM - 3:00 PM", "3:00 PM - 4:00 PM", "4:00 PM - 5:00 PM", "5:00 PM - 6:00 PM", "6:00 PM - 7:00 PM"];
 
         const getHourGroup = (timestamp: string): string => {
@@ -211,6 +217,9 @@ function processProductionData(productionData: ProductionDataForChartTypes[]): O
             categories,
             machines,
             eliot,
+             low:obbSheet?.efficiencyLevel1,
+            // mid:obbSheet?.efficiencyLevel2
+            high:obbSheet?.efficiencyLevel3
 
 
         };
@@ -225,7 +234,7 @@ function processProductionData(productionData: ProductionDataForChartTypes[]): O
             const response : any = await fetchDirectProductionData(data.obbSheetId, formattedDate);
 
 
-            const heatmapData  = processProductionData(response.data);
+            const heatmapData  = processProductionData(response.data,response.obbSheet);
             
             setHeatmapData(heatmapData);
             setObbSheet(response.obbSheet);
