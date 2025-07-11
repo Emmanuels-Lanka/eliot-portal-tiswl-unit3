60e7f316c0849f6be02e1ca06818628114f7acc5
ImAadhi <adilsaaly@gmail.com>
2025-07-12 00:35:22 +0530

    build done

--- app/(tv-charts)/overall-eff/[linename]/_components/hmap-15.tsx
+++ app/(tv-charts)/overall-eff/[linename]/_components/hmap-15.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 import { Cog } from "lucide-react";
 import { getObbSheetID } from "@/components/tv-charts/achievement-rate-operation/actions";
 import BarChartGraphEfficiencyRate from "@/app/(dashboard)/analytics/efficiency-rate/_components/bar-chart-graph";
+import { fetchDirectProductionData } from "@/actions/efficiency-direct-action";
+import { finalDataTypes } from "@/app/(dashboard)/analytics/efficiency-rate/_components/analytics-chart";
 //import HmapChart15Compo from "@/app/(dashboard)/analytics/operation-efficiency-15/_components/heatmap-15-min";
 //import HmapChart15Compo from "@/app/(dashboard)/analytics/operation-efficiency-15/_components/heatmap-15-min";
 // import HmapChart15Compo from "@/app/(dashboard)/analytics/operator-efficiency-15/_components/heatmap-15-min";
@@ -14,14 +16,11 @@ const Hmap15CompoOperation = ({ linename }: { linename: string }) => {
 
   const [obbSheetId, setobbSheetId] = useState<string>("")
   const [date, setdate] = useState<string>("")
+    const[finalData,setFinalData]=useState<finalDataTypes[]>([])
 
   const getObbSheetID1 = async () => {
-    const obbSheetId1 = await getObbSheetID(linename);
-    setobbSheetId(obbSheetId1)
-   
-  }
 
-  useEffect(() => {
+    setobbSheetId(linename)
 
     const y = new Date().getFullYear().toString()
     const m = (new Date().getMonth() + 1).toString().padStart(2, "0")
@@ -29,15 +28,84 @@ const Hmap15CompoOperation = ({ linename }: { linename: string }) => {
     const today = new Date();
     const yyyyMMdd = today.getFullYear() + '-' + (today.getMonth() + 1).toString().padStart(2, '0') + '-' + today.getDate().toString().padStart(2, '0');
   
-   const date =  yyyyMMdd.toString()+"%"
+   const date =  yyyyMMdd.toString()
     setdate(date)
 
-  }, [])
+
+
+    const dataa = await fetchDirectProductionData(linename,date)
+   
+    processData(dataa.data)
+  }
+
+ 
+
+    const processData = (productionData: any[]) => {
+    
+             const operationsMap: { [key: string]: any[] } = {};
+            productionData.forEach(data => {
+                if (!operationsMap[data.obbOperation?.id]) {
+                    operationsMap[data.obbOperation?.id] = [];
+                }
+                operationsMap[data.obbOperation?.id].push(data);
+            });
+    
+            const operations = Object.values(operationsMap).map(group => ({
+                obbOperation: group[0].obbOperation,
+                
+                data: group
+            })).sort((a, b) => a.obbOperation.seqNo - b.obbOperation.seqNo);
+    
+            console.log(operations)
+
+            const formattedData = operations.map((o)=>{
+               const log =  o.data[0].operator.operatorSessions?.find((s:any)=>s.obbOperationId === o.obbOperation.id )?.LoginTimestamp
+               const loginTime = new Date(log);
+               const  lastProductionTime = o.data[0].timestamp;
+               const lastTime = new Date(lastProductionTime)
+               let timeDiffMinutes = (lastTime.getTime() - loginTime.getTime()) / (1000 * 60);
+               let is2Passed :boolean = false
+                  let isLoggedBfr2 :boolean =false
+               if (
+                 lastTime.getHours() > 14 ||
+                 (lastTime.getHours() === 14 && lastTime.getMinutes() >= 5)
+               ) {
+                 if (
+                   loginTime.getHours() < 14 ||
+                   (loginTime.getHours() === 14 && loginTime.getMinutes() < 5)
+                 ) {
+                   timeDiffMinutes -= 60;
+                   isLoggedBfr2 = true;
+                 }
+                 is2Passed = true;
+               }
+
+
+                const smv = o.obbOperation.smv
+                const lastProd = o.data[0].totalPcs
+                const earnMins = smv* lastProd
+                const efficiency = timeDiffMinutes > 0  ? Math.max(Math.min((earnMins * 100) / timeDiffMinutes, 100), 0)  : 0
+                return{
+                    operation: o.data[0].obbOperation.seqNo+"-"+o.obbOperation.operation.name,
+                    operator:o.data[0].operator.name,
+                    
+                    efficiency : Number(Math.round(efficiency))
+                }
+            })
+
+            console.log(formattedData,"fd")
+            setFinalData(formattedData)
+            
+          }
+
+
 
   useEffect(() => {
      
     getObbSheetID1()
   }, [linename])
+
+
   return (
     <div className="h-[200]">
       <div className='flex justify-center items-center gap-3'>
@@ -48,6 +116,7 @@ const Hmap15CompoOperation = ({ linename }: { linename: string }) => {
       {obbSheetId.length > 0 ? <BarChartGraphEfficiencyRate
         obbSheetId={obbSheetId}
         date={date}
+        finalData={finalData}
       /> : <span>No Layout for Line {linename} - {date}</span>}
     </div>
   )
